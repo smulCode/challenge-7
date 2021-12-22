2c7ca39003c68d0a1619f88ab66c6883d1919085
smulCode <67417862+smulCode@users.noreply.github.com>
2021-12-22 14:58:59 +0100

    challenge v1

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,23 @@
-
 // import {example} from "./modules/example.js"
 
+const firstName = document.getElementById("fname");
+const phoneNumber = document.getElementById("phone");
+const streetName = document.getElementById("streetname");
+const houseNumber = document.getElementById("housenumber");
+const zipCode = document.getElementById("zipcode");
+const city = document.getElementById("city");
+const email = document.getElementById("email");
+const sendButton = document.getElementById("btn-send");
+
+sendButton.addEventListener("click",getInput)
+
+function getInput() {
+  document.querySelector(".name").innerText = `name: ${firstName.value}`;
+  document.querySelector(".phonenumber").innerText = `phone number: ${phoneNumber.value}`;
+  document.querySelector(".adress").innerText = 
+  `adress: ${streetName.value} ${houseNumber.value} ${zipCode.value} ${city.value}`;
+
+  document.querySelector(".email").innerText = `email: ${email.value}`;
 
 
-
+}
