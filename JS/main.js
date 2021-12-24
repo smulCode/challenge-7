49236f88a0c20161a87e1d70664a4699d610f10f
smulCode <67417862+smulCode@users.noreply.github.com>
2021-12-24 08:29:03 +0100

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -12,16 +12,23 @@ const form = document.getElementById("form");
 const card = document.getElementById("card");
 const containerCard = document.getElementById("container-card");
 
-
 const imgContainer = document.createElement("div");
 imgContainer.classList.add("img-container");
 const image = document.createElement("img");
-image.classList.add("picture")
-containerCard.appendChild(imgContainer)
-imgContainer.appendChild(image)
+image.classList.add("picture");
+containerCard.appendChild(imgContainer);
+imgContainer.appendChild(image);
 
-class user  {
-    constructor(fullName,phoneNumber,streetName,houseNumber,zipCode,city,email){
+class user {
+  constructor(
+    fullName,
+    phoneNumber,
+    streetName,
+    houseNumber,
+    zipCode,
+    city,
+    email
+  ) {
     this.fullName = fullName;
     this.phoneNumber = phoneNumber;
     this.streetName = streetName;
@@ -29,79 +36,75 @@ class user  {
     this.zipCode = zipCode;
     this.city = city;
     this.email = email;
-}
+  }
 }
 
-sendButton.addEventListener("click",getInput)
+sendButton.addEventListener("click", getInput);
 
 function getInput() {
-  
-  validateForm()
+  validateForm();
   document.querySelector(".name").innerText = `Name: ${fullName.value}`;
 
- let nr = phoneNumber.value.toString()
-  if(phoneNumber.value.charAt(0) == 0){
-    document.querySelector(".phonenumber").innerText = `Phone number: ${nr.replace('0', '+31')}`
-  }else{ document.querySelector(".phonenumber").innerText = `Phone number: ${nr}`}
-  ;
-
-  document.querySelector(".adress").innerText = 
-  `Adress: ${streetName.value} ${houseNumber.value} ${zipCode.value} ${city.value}`;
+  let nr = phoneNumber.value.toString();
+  if (phoneNumber.value.charAt(0) == 0) {
+    document.querySelector(
+      ".phonenumber"
+    ).innerText = `Phone number: ${nr.replace("0", "+31")}`;
+  } else {
+    document.querySelector(".phonenumber").innerText = `Phone number: ${nr}`;
+  }
+  document.querySelector(
+    ".adress"
+  ).innerText = `Adress: ${streetName.value} ${houseNumber.value} ${zipCode.value} ${city.value}`;
 
   let emailAddress = email.value;
-  if (emailAddress.includes('@techgrounds')){
-      document.querySelector(".email").innerText = `email: ${emailAddress}`;
-  }else{ 
+  if (emailAddress.includes("@techgrounds")) {
+    document.querySelector(".email").innerText = `email: ${emailAddress}`;
+  } else {
     let message = "Please enter email with @techgrounds.com";
     let result = window.confirm(message);
   }
-  saveInput()
-
+  saveInput();
 }
 
+function saveInput() {
+  const user1 = new user(
+    fullName.value,
+    phoneNumber.value,
+    streetName.value,
+    houseNumber.value,
+    zipCode.value,
+    city.value,
+    email.value
+  );
+  console.log(user1);
 
+  showImage(user1);
 
-
-function saveInput(){
-    const user1 = new user(fullName.value,phoneNumber.value,streetName.value,houseNumber.value,zipCode.value,city.value,email.value);
-    console.log(user1);
-
-    
-    showImage(user1)
-    
-    fullName.value = "";
-    phoneNumber.value = "";
-    streetName.value = "";
-    houseNumber.value = "";
-    zipCode.value = "";
-    city.value = "";
-    email.value = "";
-    
-    
-    
-
+  fullName.value = "";
+  phoneNumber.value = "";
+  streetName.value = "";
+  houseNumber.value = "";
+  zipCode.value = "";
+  city.value = "";
+  email.value = "";
 }
 
-function showImage(user){
-
-
-  if(fullName.value  == "" || fullName.value  == null){
+function showImage(user) {
+  if (fullName.value == "" || fullName.value == null) {
     console.log(fullName.value);
-    imgContainer.classList.remove("image-show")
-  } else{ 
-  
-    imgContainer.classList.add("image-show")
-    image.src = "https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80";
-    console.log("show image");
+    imgContainer.classList.remove("image-show");
+  } else {
+    imgContainer.classList.add("image-show");
+    image.src =
+      "https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80";
+    
   }
- 
-
-
 }
 
 function validateForm() {
   const formValues = document.forms["myForm"]["fullname"].value;
-  if (formValues  == "" || formValues  == null) {
+  if (formValues == "" || formValues == null) {
     alert("Name must be filled out");
     return false;
   }
